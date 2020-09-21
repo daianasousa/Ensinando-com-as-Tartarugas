b6ef984181a69c9906388c44fc5f9b3564d7d29e
daianasousa <daianasousa04121996@gmail.com>
2020-09-21 20:27:53 -0300

    desenhando estrelas

--- Desenhando_estrelas.py
+++ Desenhando_estrelas.py
@@ -1,19 +1,28 @@
 from turtle import *
 
+#uma função para desenhar uma estrela
+#'def' signica 'define'
+def drawStar():
+    pendown()
+    begin_fill()
+    for side in range(5):
+        left(144)
+        forward(50)
+    end_fill()
+    penup()
+
+
 #isso vai desenhar uma estrela cinza clara em um fundo azul escuro
 color("WhiteSmoke")
 bgcolor("MidnightBlue")
 
-pendown()
-begin_fill()
-
-#desenha a forma da estrela
-for side in range(5):
-    left(144)
-    forward(50)
-
-end_fill()
-penup()
-
+#use a função para desenhar estrelas!
+drawStar()
 forward(100)
+drawStar()
+left(120)
+forward(150)
+drawStar()
+
+hideturtle()
 done()
