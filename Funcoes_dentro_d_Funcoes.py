fbff3daf16b1b0e2933787bc6a59c141a8658a58
daianasousa <daianasousa04121996@gmail.com>
2020-09-23 16:27:13 -0300

    Create Funcoes_dentro_d_Funcoes.py

---
+++ Funcoes_dentro_d_Funcoes.py
@@ -0,0 +1,68 @@
+from turtle import *
+from random import * 
+
+#uma função para mover a tartaruga para uma posição aleatória
+def moveToRandomLocation():
+    penup()
+    setpos( randint(-400,400) , randint(-400,400))
+    pendown()
+    
+#uma função para desenhar uma estrela de um tamanho específico
+def drawStar(starSize, starColour):
+    color(starColour)
+    pendown()
+    begin_fill()
+    for side in range(5):
+        left(144)
+        forward(starSize)
+    end_fill()
+    penup()
+
+#uma função para desenhar uma pequena galáxia de estrelas
+def drawGalaxy(number0fStars):
+    starColours = ["#058396","#0272A6","#827E01"]
+    moveToRandomLocation()
+    #desenha várias pequenas estrelas coloridas
+    for star in range(number0fStars):
+        penup()
+        left( randint(-180,180))
+        forward( randint(5,20))
+        pendown()
+        #desenha uma pequena estrela de cor aleatória
+        drawStar( 2, choice(starColours))
+
+#uma função para desenhar uma constelação de estrelas
+def drawConstellation(number0fStars):
+    moveToRandomLocation()
+    #primeiro desenhamos todas as estrelas, exceto a última,
+    #conectadas por linhas, assim: *--*--*--
+    for star in range(number0fStars-1):
+        drawStar( randint(7,15) , "white")
+        pendown()
+        left( randint(-90,90))
+        forward( randint(30,70))
+    #agora desenhamos a última estrela
+    drawStar( randint(7,17) , "white")
+
+speed(11)
+
+
+#isso desenha um fundo azul escuro
+bgcolor("MidnightBlue")
+
+#desenha 30 estrelas brancas (tamanho,posições aleatórias)
+for star in range(30):
+    moveToRandomLocation()
+    drawStar( randint(5,25) , "White")
+    
+#desenha 3 pequenas galáxias de 40 estrelas
+for galaxy in range(3):
+    drawGalaxy(40)
+
+#desenha 2 constelações, cada uma com um número aleatório de estrelas
+for constellation in range(2):
+    drawConstellation(randint(4,7))
+    
+
+hideturtle()
+done
